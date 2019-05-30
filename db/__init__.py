70f32a6bff25ba25b88a27a8f3174644427a25b7
liangyy75 <3249475320@qq.com>
2019-05-30 22:45:51 +0800

    update db __init__

--- db/__init__.py
+++ db/__init__.py
@@ -2,3 +2,10 @@ __all__ = ["prepare", "DbHelper", "Student", "Organization", "Task", "Accept", "
 
 from .prepare import db, app, model_repr
 from .DbHelper import db_helper
+
+from .Student import Student, random_stus
+from .Organization import Organization, random_orgs
+from .Task import Task, random_tasks
+from .Accept import Accept, random_accepts
+from .Problem import Problem, random_problems
+from .Answer import Answer, random_answers
