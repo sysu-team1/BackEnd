594f2d8cc64e8a261df210f32a1c49547dd21d35
dream_fly_runner <1014040848@qq.com>
2019-05-31 21:17:53 +0800

    后台获取任务

--- responses/get_tasks.py
+++ responses/get_tasks.py
@@ -1,21 +1,21 @@
+from db import db_helper, app, model_repr
 
-time_stamp = 'time_stamp'
 published_id = 'published_id'
 accepter_id = 'accepter_id'
 tag = 'tag'
-title_or_content = 'title_or_content'
-
+text = 'text'
 
 def get_tasks_by_(args):
 	res = {}
 	if(time_stamp in args):
-		pass
+		db_helper.get_task(last_id = args.get('last_id'))
 	elif(published_id in args):
-		pass
+		tasks = db_helper.get_publish_tasks(args.get(published_id), last_id = args.get('last_id'))
 	elif(accepter_id in args):
-		pass
+		tasks = db_helper.get_accept_tasks(args.get(accepter_id), last_id = args.get('last_id'))
 	elif(tag in args):
-		pass
-	elif(title_or_content in args):
-		pass
+		tasks = db_helper.get_task_by_tag(args.get(tag), last_id = args.get('last_id'))
+	elif(text in args):
+		tasks = db_helper.get_task_by_text(args.get(text), last_id = args.get('last_id'))
+	res = {'error': 0, 'data': {'msg': '获取成功', 'tasks': tasks}}
 	return str(res)
